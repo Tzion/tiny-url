d40accd1a242b1efe96c44c58ac45213576d5fd5
Tzion <tzion12345@gmail.com>
2022-05-17 17:16:25 +0300

    add database.Insert

--- database/database.go
+++ database/database.go
@@ -1,5 +1,9 @@
 package database
 
+import (
+	"log"
+)
+
 type database struct {
 	shortToLongUrl map[string]string
 }
@@ -10,6 +14,10 @@ func (db database) init() {
 	Database.shortToLongUrl = make(map[string]string)
 }
 
+func (db database) len() int {
+	return len(Database.shortToLongUrl)
+}
+
 func (db database) FetchLongUrl(shortUrl string) *string {
 	longUrl, exists := db.shortToLongUrl[shortUrl]
 	if exists {
@@ -17,4 +25,17 @@ func (db database) FetchLongUrl(shortUrl string) *string {
 	} else {
 		return nil
 	}
+}
+
+func (db database) Insert(shortUrl string, longUrl string) {
+	currentLongUrl, exists := db.shortToLongUrl[shortUrl]
+	if exists {
+		if currentLongUrl == longUrl {
+			return
+		} else {
+			log.Panicf("Collision upon entry %s. Overriding with latest value", shortUrl)
+			db.shortToLongUrl[shortUrl] = longUrl
+		}
+	}
+	db.shortToLongUrl[shortUrl] = longUrl
 }
--- database/database_test.go
+++ database/database_test.go
@@ -4,9 +4,21 @@ import (
 	"testing"
 )
 
-func TestDatabase(t *testing.T) {
+func TestDatabaseInit(t *testing.T) {
+	Database.init()
+	if Database.shortToLongUrl == nil {
+		t.Error("Database is not initialize")
+	}
+	if Database.len() != 0 {
+		t.Error("Database is not empty")
+	}
+}
+
+func TestInsert(t *testing.T) {
+	Database.init()
+	t.Log(Database.len())
+	Database.Insert("shortUrl", "longUrl")
 	t.Log(Database)
 	Database.init()
-	Database.shortToLongUrl["a"] = "A"
 	t.Log(Database)
 }
