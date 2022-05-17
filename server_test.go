908c4e36b81b0b01ae6d1fec7319c2b4604801f4
Tzion <tzion12345@gmail.com>
2022-05-18 00:26:25 +0300

    more coverage for server tests

--- server_test.go
+++ server_test.go
@@ -13,14 +13,14 @@ func init() {
 }
 
 func TestRedirectToStoredUrl(t *testing.T) {
-	tinyUrl, url := store("https://recolabs.dev/")
+	tinyUrl, _ := store("https://recolabs.dev/")
 	resp, err := http.Get("http://localhost:8080/" + tinyUrl)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(resp.StatusCode)
-	t.Log(resp)
-	t.Log(url)
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Redirection failed")
+	}
 }
 
 // TODO break down this flow to multiple test cases
@@ -43,6 +43,17 @@ func TestPostNewEntry(t *testing.T) {
 		t.Fatalf("Entry for short-url %q does not exsits in the database", shortUrl)
 	}
 	if *entry != url {
-		t.Errorf("stored url does not match the original url\nhave %s\nwant %s", *entry, url)
+		t.Fatalf("stored url does not match the original url\nhave %s\nwant %s", *entry, url)
 	}
+}
+
+func TestRequestFaultyTinyUrl(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/" + "NowsGod6GUI=")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !(400 <= resp.StatusCode && resp.StatusCode < 500) {
+		t.Fatalf("Expecting status code 4XX, instread recieved %d", resp.StatusCode)
+	}
+
 }
