e8aa23e9be50ea2c950352dfcb08451da9a89fb4
Tzion <tzion12345@gmail.com>
2022-05-17 13:11:14 +0300

    fix tests of hashing

--- hash_test.go
+++ hash_test.go
@@ -12,17 +12,10 @@ var urls = []string{
 	"https://gobyexample.com/base64-encoding",
 }
 
-func TestShortUrlLength(t *testing.T) {
-	shortUrl := ShortenUrl("https://gobyexample.com/base64-encoding")
-	if urlLen, encodedLen := len(shortUrl), hashMethod.Size(); urlLen != encodedLen*8/6 {
-		t.Errorf("length of short url is wrong\nhave %d\nwant %d", urlLen, encodedLen)
-	}
-}
-
 func TestHashedUrlLength(t *testing.T) {
 	for _, url := range urls {
 		t.Run(string(url), func(t *testing.T) {
-			shortUrl := hashUrl("https://gobyexample.com/base64-encoding")
+			shortUrl := hashUrl(url)
 			if urlLen, hashLen := len(shortUrl), hashMethod.Size(); urlLen != hashLen {
 				t.Errorf("length of short url is wrong\nhave %d\nwant %d", urlLen, hashLen)
 			}
@@ -32,7 +25,6 @@ func TestHashedUrlLength(t *testing.T) {
 func TestDeterminism(t *testing.T) {
 	for _, url := range urls {
 		t.Run(string(url), func(t *testing.T) {
-			url := "https://golangr.com/golang-http-server/"
 			if first, second := ShortenUrl(url), ShortenUrl(url); first != second {
 				t.Errorf("2 calls over the same url generate different results\nfirst=%s\nsecond=%s", first, second)
 			}
